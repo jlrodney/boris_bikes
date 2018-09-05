831a79788b01828d3dedbf4a851f9a2e57673b21
James <jamie@Shelis-MacBook-Air.local>
2018-09-05 15:03:26 +0100

    ex 1 -12

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,4 +1,24 @@
+require_relative 'bike'
 class DockingStation
+
+attr_reader :bike
+
+def initialize
+  @bike = Bike.new
+end
+
   def release_bike
+    @bike
+  end
+
+  def has_bike
+    if bikes_available < 1
+   fail "No bikes available!!"
+    end
+  end
+
+
+  def dock_bike(bike)
+    @bike = bike
   end
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,8 +1,21 @@
-expect(actual).to be_kind_of(expected)
+
 
 require 'DockingStation'
 require 'Bike'
 describe DockingStation do
+
+  it "it true" do
+    expect { raise "No bikes here" }.to raise_error
+  end 
+
   it { should respond_to(:release_bike) }
-  it {should to be_kind_of(Bike) }
+
+  it 'releases working bikes' do
+
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
+
+  it {should respond_to(:dock_bike) }
+
 end
